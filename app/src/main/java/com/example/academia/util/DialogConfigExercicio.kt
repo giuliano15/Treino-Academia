03c49fe3812efc4dc31cd5485f4732ae1dd3ff07
Giuliano <giulsilva@hotmail.com>
2023-12-17 18:03:20 -0300

    melhora no Dialog config exercicios

--- app/src/main/java/com/example/academia/util/DialogConfigExercicio.kt
+++ app/src/main/java/com/example/academia/util/DialogConfigExercicio.kt
@@ -1,19 +1,22 @@
-package com.example.academia.util
+package com.example.academia.view
 
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.academia.R
-
+import com.example.academia.util.setFilterOnlyNumbers
 
 class DialogConfigExercicio {
     companion object {
         fun showConfigurarExercicioDialog(
             context: Context,
             title: String,
-            onPositiveClick: (String, String, String,String,String) -> Unit,
+            onPositiveClick: (String, String, String, String, String) -> Unit,
             onNegativeClick: () -> Unit
         ) {
             val dialogView =
@@ -22,8 +25,12 @@ class DialogConfigExercicio {
             val nomeTreino = dialogView.findViewById<EditText>(R.id.editTextNomeTreino)
             val cargaEditText = dialogView.findViewById<EditText>(R.id.editTextCarga)
             val tempoEditText = dialogView.findViewById<EditText>(R.id.editTextTempo)
-            val quantidadeSeriesEditText = dialogView.findViewById<EditText>(R.id.editTextQuantidadeSeries)
+            val quantidadeSeriesEditText =
+                dialogView.findViewById<EditText>(R.id.editTextQuantidadeSeries)
             val observacacao = dialogView.findViewById<EditText>(R.id.editTextObservacao)
+            val titleTextView = dialogView.findViewById<TextView>(R.id.title)
+            val btnAdd = dialogView.findViewById<Button>(R.id.btnAdd)
+            val btnCancel = dialogView.findViewById<Button>(R.id.btnCancel)
 
             cargaEditText.setFilterOnlyNumbers()
             tempoEditText.setFilterOnlyNumbers()
@@ -31,12 +38,11 @@ class DialogConfigExercicio {
 
             val builder = AlertDialog.Builder(context)
                 .setView(dialogView)
-                .setPositiveButton("Adicionar", null)  // Ouve o clique, mas não fecha o diálogo
 
             val alertDialog = builder.create()
 
             alertDialog.setOnShowListener {
-                val positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                val positiveButton = btnAdd
                 positiveButton.setOnClickListener {
                     val nomeTreino = nomeTreino.text.toString()
                     val carga = cargaEditText.text.toString()
@@ -46,22 +52,29 @@ class DialogConfigExercicio {
 
                     // Verifica se os campos não estão vazios
                     if (carga.isNotEmpty() && tempo.isNotEmpty() && quantidadeSeries.isNotEmpty()) {
-                        onPositiveClick(nomeTreino,carga, tempo, quantidadeSeries,observacao)
+                        onPositiveClick(nomeTreino, carga, tempo, quantidadeSeries, observacao)
                         alertDialog.dismiss()  // Fecha o diálogo após o clique positivo
                     } else {
                         // Exibe uma mensagem de erro se algum campo estiver vazio
-                        Toast.makeText(context, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context,
+                            "Preencha todos os campos",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
+                }
+
+                val negativeButton = btnCancel
+                negativeButton.setOnClickListener {
+                    onNegativeClick()
+                    alertDialog.dismiss()
                 }
             }
 
-            builder.setNegativeButton("Cancelar") { _, _ ->
-                onNegativeClick()
-            }
+            // Configurar o título principal
+            titleTextView.text = title
 
             alertDialog.show()
         }
     }
 }
-
-
